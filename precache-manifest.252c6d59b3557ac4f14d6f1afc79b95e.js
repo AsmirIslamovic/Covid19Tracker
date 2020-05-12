62ae8f38378dd766c561a12533295210f0b6f9a8
Asmir Islamovic <asmir.islamovic@mail.utoronto.ca>
2020-05-11 22:39:13 -0400

    Updates

---
+++ precache-manifest.252c6d59b3557ac4f14d6f1afc79b95e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dde84a4d48eb9a7ad322863d8fb633",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "762129d1bccf28233c81",
+    "url": "/Covid19Tracker/static/css/main.3743f649.chunk.css"
+  },
+  {
+    "revision": "74de9119af14aa5421bc",
+    "url": "/Covid19Tracker/static/js/2.facb796e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.facb796e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762129d1bccf28233c81",
+    "url": "/Covid19Tracker/static/js/main.b8ed6c1d.chunk.js"
+  },
+  {
+    "revision": "d63efc3f614e25f2e56f",
+    "url": "/Covid19Tracker/static/js/runtime-main.43cd3b26.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
